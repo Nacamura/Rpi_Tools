1e099716b1bd73f586984741f92b04d711ba2551
Nacamura <t.nacamura@gmail.com>
2014-04-16 23:28:39 +0900

    remove pocket

--- TweetBookMark.rb
+++ TweetBookMark.rb
@@ -2,7 +2,6 @@ require 'json'
 load 'MyLogger.rb'
 load 'MyTwitter.rb'
 load 'Instapaper.rb'
-load 'Pocket.rb'
 
 class TweetBookMark
   include MyLogger
@@ -14,7 +13,6 @@ class TweetBookMark
     urls = twitter.gather_hatebu_urls_without_ng(ng_words).reverse!
     new_urls = urls.reject {|url| stored_urls.include? url}
     Instapaper.new(settings).add_all(new_urls)
-    Pocket.new(settings).add_all(new_urls)
     MyJSON.store_json(urls, "urls.txt")
     twitter.dump_recorded
   end
