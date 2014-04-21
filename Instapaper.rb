78c191f7acbf1e9cacbb62251c554ba1e225c0b8
Nacamura <t.nacamura@gmail.com>
2014-04-21 23:33:39 +0900

    remove unused hatebu methods

--- Instapaper.rb
+++ Instapaper.rb
@@ -23,21 +23,4 @@ class Instapaper
 		end
 	end
 
-	def add_with_hatebu(url)
-		res = RestClient.post("https://www.instapaper.com/api/add",
-			"username"=>@user, "password"=>@password, "url"=>url)
-		if res != "201" then return res end
-		location = res.headers[:content_location]
-		sleep 1
-		add("http://b.hatena.ne.jp/entry/" + location)
-	end
-
-	def add_all_with_hatebu(urls)
-		@logger.debug("add " + urls.length.to_s + " URLs")
-		urls.each do |url|
-			res = add_with_hatebu(url)
-			if res != "201" then @logger.warn(url + " : " + res) end
-			sleep 1
-		end
-	end
 end
