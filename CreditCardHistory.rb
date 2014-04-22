bb0737dbcb9b0bbc0fac22c89152ac788d4daf29
Nacamura <t.nacamura@gmail.com>
2014-04-22 23:29:01 +0900

    remove DropboxUpload

--- CreditCardHistory.rb
+++ CreditCardHistory.rb
@@ -1,18 +1,9 @@
 load 'NetAnswer.rb'
 load 'VPass.rb'
-load 'DropboxUtil.rb'
 
 class CreditCardHistory
-
   def call
     NetAnswer.new.call
     VPass.new.call
-    dropbox = DropboxUtil.new
-    Dir.new('./').each do |file|
-      if(File.extname(file) == ".csv")
-      	dropbox.upload('/', './', file)
-      end
-    end
   end
-  
 end
