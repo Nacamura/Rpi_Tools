aae70e6ac2abd5d19a9f10a52bc407881607d4fd
Nacamura <t.nacamura@gmail.com>
2014-04-26 23:41:43 +0900

    able to choose archive dir & not to archive

--- DaemonClock.rb
+++ DaemonClock.rb
@@ -13,7 +13,7 @@ module Clockwork
   end
 
   #Log Upload jobs
-  every(1.day, DropboxUploader.new(".log"), :at=>'06:30')
+  every(1.day, DropboxUploader.new(".log", "non_archive"), :at=>'06:30')
 
   #TrainSearch jobs
   every(2.minute, (@twitdaemon ||= TwitDaemon.new), :if=>lambda{|t| (10...24) === t.hour})
@@ -23,11 +23,11 @@ module Clockwork
 
   #CreditCardHistory jobs
   every(1.day, CreditCardHistory.new, :if=>lambda{|t| t.day == 25}, :at=>'05:00')
-  every(1.day, DropboxUploader.new(".csv"), :if=>lambda{|t| t.day == 25}, :at=>'06:00')
+  every(1.day, DropboxUploader.new(".csv", "CCH_archive"), :if=>lambda{|t| t.day == 25}, :at=>'06:00')
 
   #Radio jobs
   every(1.week, Radio.new("Radiko", "TBS", "120", "0100", "Baka"), :at=>'Tuesday 00:59')
   every(1.week, Radio.new("AandG", "Anigera", "90", "2100", "Anigera"), :at=>'Thursday 20:59')
   every(1.week, Radio.new("AandG", "AoiSaori", "30", "2330", "AoiSaori"), :at=>'Wednesday 23:29')
-  every(1.hour, DropboxUploader.new(".mp3"), :at=>['06:00', '22:15'])
+  every(1.hour, DropboxUploader.new(".mp3", "mp3_archive"), :at=>['06:00', '22:15'])
 end
--- DropboxUploader.rb
+++ DropboxUploader.rb
@@ -2,16 +2,17 @@ load 'DropboxUtil.rb'
 require 'fileutils'
 
 class DropboxUploader
-	def initialize(extname)
+	def initialize(extname, archive_dir)
 		@extname = extname
 		@dropbox = DropboxUtil.new
+		@archive_dir = archive_dir
 	end
 
 	def call
 		Dir.new('./').each do |file|
 			if(File.extname(file) == @extname)
 				@dropbox.upload('/', './', file)
-				FileUtils.mv(file, "archive")
+				FileUtils.mv(file, @archive_dir) if (Dir.exists? @archive_dir)
 			end
 		end
 	end
