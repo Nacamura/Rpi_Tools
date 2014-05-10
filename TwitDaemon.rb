e28042117fdc9427f23f30ea016ea18fc8d8e9cd
Nacamura <t.nacamura@gmail.com>
2014-05-10 23:57:31 +0900

    refactor to use split instead of lines

--- TwitDaemon.rb
+++ TwitDaemon.rb
@@ -13,10 +13,7 @@ class TwitDaemon
     @twitcom ||= TwitCommunicator.new( MyJSON.load_json("settings.txt") )
     dms = @twitcom.gather_new_direct_messages
     dms.each do |dm|
-      lines = []
-      dm.text.lines do |l|
-        lines << l
-      end
+      lines = dm.text.split("\n")
       if(lines[0].strip != "pi")
         next
       else
