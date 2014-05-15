5f9c910148b36b61857454c5b2b9a5ba6d536198
Nacamura <t.nacamura@gmail.com>
2014-05-15 23:47:48 +0900

    add NanacoDepoChecker

---
+++ NanacoDepoChecker.rb
@@ -0,0 +1,32 @@
+load 'MyGmail.rb'
+
+class NanacoDepoChecker
+
+  def call
+    auth = open('Nanaco_Auth.txt') {|i| JSON.load(i)}
+    if(get_deposit(get_mechanize_res(auth)) < 1000)
+      MyGmail.new.send('NanacoDeposit', '残高が少なくなっています')
+    end
+  end
+
+  def get_mechanize_res(auth)
+    agent = Mechanize.new
+    agent.user_agent_alias = 'Mac Safari'
+    login = agent.get ''
+    login_form = login.form
+    login_form.field_with(:name=>'id').value = auth['id']
+    login_form.field_with(:name=>'password').value = auth['password']
+    mypage = login_form.click_button
+  end
+
+  def get_deposit(mechanize_res)
+    deposit = 1000000
+    mechanize_res.search('tr').each do |tr|
+      if(tr.some_extraction == 'money')
+        deposit = money_gsub
+      end
+    end
+    deposit
+  end
+
+end
