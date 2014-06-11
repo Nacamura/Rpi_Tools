19733e7e2735186625d0b60b8d10bba9a78ddaec
Nacamura <t.nacamura@gmail.com>
2014-06-11 23:49:26 +0900

    Stop logging for added URL count

--- Instapaper.rb
+++ Instapaper.rb
@@ -1,10 +1,8 @@
 require 'restclient'
 
 class Instapaper
-	include MyLogger
 
 	def initialize(settings)
-		@logger = get_logger
 		@user = settings["instapaper_user"]
 		@password = settings["instapaper_pw"]
 	end
@@ -15,7 +13,6 @@ class Instapaper
 	end
 
 	def add_all(urls)
-		@logger.debug("add " + urls.length.to_s + " URLs")
 		urls.each do |url|
 			add(url)
 			sleep 1
