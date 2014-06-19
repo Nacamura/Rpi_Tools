d2c99d69baa43cfe338d24c2f02b98b1a2afe38f
Nacamura <t.nacamura@gmail.com>
2014-06-19 23:59:33 +0900

    remove comments

--- DaemonClock.rb
+++ DaemonClock.rb
@@ -14,29 +14,22 @@ module Clockwork
   	job.call
   end
 
-  #Log Upload jobs
   every(1.hour, DropboxUploader.new(".log", "non_archive"), :at=>['08:30', '20:30'])
 
-  #TrainSearch jobs
   every(3.minute, (@twitdaemon ||= TwitDaemon.new), :if=>lambda{|t| (16..19) === t.hour})
 
-  #tweetbookmark jobs
   every(1.hour, TweetBookMark.new, :at=>'**:55')
 
-  #CreditCardHistory jobs
   every(1.day, CreditCardHistory.new, :if=>lambda{|t| t.day == 20}, :at=>'05:00')
   every(1.day, DropboxUploader.new(".csv", "CCH_archive"), :if=>lambda{|t| t.day == 20}, :at=>'07:00')
 
-  #NanacoDepoChecker jobs
   every(1.day, NanacoDepoChecker.new, :at=>'20:00')
 
-  #Radio jobs
   every(1.week, Radio.new("Radiko", "TBS", "120", "0100", "Baka"), :at=>'Tuesday 00:59')
   every(1.week, Radio.new("AandG", "Anigera", "90", "2100", "Anigera"), :at=>'Thursday 20:59')
   every(1.week, Radio.new("AandG", "AoiSaori", "30", "2330", "AoiSaori"), :at=>'Wednesday 23:29')
   every(1.hour, DropboxUploader.new(".mp3", "mp3_archive"), :at=>['06:00', '22:15'])
 
-  #RadioFailChecker jobs
   every(1.day, RadioFailChecker.new, :at=>'22:30')
 
 end
